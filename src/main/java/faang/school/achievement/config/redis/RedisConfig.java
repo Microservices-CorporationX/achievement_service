d55717e3fa2eb9aee30e4d5a5a4a2ea8242076ac
Nikolay Orlenko <norlenko2801@gmail.com>
2024-12-13 00:04:23 +0300

    исправили циклическую зависимость

--- src/main/java/faang/school/achievement/config/redis/RedisConfig.java
+++ src/main/java/faang/school/achievement/config/redis/RedisConfig.java
@@ -1,12 +1,10 @@
-package faang.school.achievement.config;
+package faang.school.achievement.config.redis;
 
 import faang.school.achievement.message.consumer.MentorshipEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.Cache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -20,16 +18,6 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.channel.mentorship}")
     private String mentorshipEventTopicName;
-
-    @Bean
-    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        return RedisCacheManager.create(redisConnectionFactory);
-    }
-
-    @Bean
-    public Cache cache(RedisCacheManager redisCacheManager) {
-        return redisCacheManager.getCache("achievementCache");
-    }
 
     @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
