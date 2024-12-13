ec9b37920a5a936ecc933fdffb8b53c0909797d5
Sanasar1 <almasyansb@gmail.com>
2024-12-13 15:51:09 +0300

    feat(achievement): добавил методы для обработки логики с ачивкой сенсей

--- src/main/java/faang/school/achievement/message/handler/mentorship/SenseiAchievementHandler.java
+++ src/main/java/faang/school/achievement/message/handler/mentorship/SenseiAchievementHandler.java
@@ -3,11 +3,13 @@ package faang.school.achievement.handler.mentorship;
 import faang.school.achievement.message.event.MentorshipEvent;
 import faang.school.achievement.service.AchievementService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SenseiAchievementHandler extends MentorshipEventHandler {
 
     private final AchievementService achievementService;
@@ -17,6 +19,7 @@ public class SenseiAchievementHandler extends MentorshipEventHandler {
 
     @Override
     public void handleEvent(MentorshipEvent event) {
+        log.info("Starting handleEvent for SenseiAchievement");
         achievementService.handleAchievement(event.userId(), senseiCacheKey);
     }
 }
