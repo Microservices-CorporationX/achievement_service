0ecab280f5f3392579ba3c1d72b4359476749267
Andrew <andrew@MPro.local>
2024-12-14 14:07:06 +0700

    Feature / Redis Cache / Achievement Service
    Adds Redis Template and Container config;
    Moves ObjectMapper config to AppConfig class;

---
+++ src/main/java/faang/school/achievement/config/redis/RedisConfig.java
@@ -0,0 +1,51 @@
+package faang.school.achievement.config.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        log.info("Connection to redis initialized at port: {}", redisProperties.port());
+        return template;
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        return container;
+    }
+
+    @Bean
+    public ChannelTopic achievementChannelTopic() {
+        return new ChannelTopic(redisProperties.channel().achievementChannel());
+    }
+
+    @Bean
+    public ChannelTopic followerChannelTopic() {
+        return new ChannelTopic(redisProperties.channel().followerChannel());
+    }
+}
---
+++ src/main/java/faang/school/achievement/config/redis/RedisProperties.java
@@ -0,0 +1,14 @@
+package faang.school.achievement.config.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+public record RedisProperties (
+        long port,
+        String host,
+        Channel channel) {
+    public record Channel(
+            String achievementChannel,
+            String followerChannel
+    ) {}
+}
