381f6afb1c18e9e102256feb71f92057b94dc025
daurenberdibekov <berder08@gmail.com>
2024-12-15 17:18:19 +0100

    Add redis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,20 @@ dependencies {
      */
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.retry:spring-retry")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+
+    /**
+     * Cache
+     */
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-cache:3.4.0")
+
 
     /**
      * Database
