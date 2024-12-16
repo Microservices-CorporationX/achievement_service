fa6963d0fbfc45f740e560c802d295d88316aa92
SerUKalinin <serukalinin@gmail.com>
2024-12-16 15:15:27 +0300

    AchievementDto

---
+++ src/main/java/faang/school/achievement/dto/achievement/AchievementDto.java
@@ -0,0 +1,19 @@
+package faang.school.achievement.dto.achievement;
+
+import faang.school.achievement.model.Rarity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AchievementDto {
+    private Long id;
+    private String title;
+    private String description;
+    private Rarity rarity;
+    private long points;
+}
