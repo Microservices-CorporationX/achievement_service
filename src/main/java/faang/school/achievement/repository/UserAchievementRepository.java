0068aa189efc5943f588d84e27c53d4c1f797cb7
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-01-10 19:26:13 +0500

    fix(41595): fix UserAchievementRepository
    Switched from CrudRepository to JpaRepository to leverage advanced JPA features. Adjusted SQL queries for compatibility with native tables and added `@Modifying` to the `giveAchievement` method to handle insert operations properly

--- src/main/java/faang/school/achievement/repository/UserAchievementRepository.java
+++ src/main/java/faang/school/achievement/repository/UserAchievementRepository.java
@@ -1,6 +1,8 @@
 package faang.school.achievement.repository;
 
 import faang.school.achievement.model.UserAchievement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,19 +10,20 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserAchievementRepository extends CrudRepository<UserAchievement, Long> {
+public interface UserAchievementRepository extends JpaRepository<UserAchievement, Long> {
 
     @Query(nativeQuery = true, value = """
                     INSERT INTO user_achievement (user_id, achievement_id)
                             VALUES (:userId, :achievementId)
                             ON CONFLICT (user_id, achievement_id) DO NOTHING;
             """)
-    boolean giveAchievement(long userId, long achievementId);
+    @Modifying
+    void giveAchievement(long userId, long achievementId);
 
     @Query(nativeQuery = true, value = """
                     SELECT CASE WHEN COUNT(ua) > 0 THEN true ELSE false END
-                    FROM UserAchievement ua
-                    WHERE ua.userId = :userId AND ua.achievement.id = :achievementId
+                    FROM user_achievement ua
+                    WHERE ua.user_id = :userId AND ua.achievement_id = :achievementId
             """)
     boolean existsByUserIdAndAchievementId(long userId, long achievementId);
 
